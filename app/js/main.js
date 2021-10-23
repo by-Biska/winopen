bdae8028dae5f252e7ff4fb6397fe1d8ca0b9325
By-Biska <varakinkirill3@gmail.com>
2021-10-23 12:15:11 +0300

    pricing

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,15 @@
 $(window).scroll(function () {
   if ($(this).scrollTop() > 10) {
-    $(".header__top").addClass('display-none');
-    $(".header__bottom").addClass('header__bottom--active');
+    $(".header__top").addClass("display-none");
+    $(".header__bottom").addClass("header__bottom--active");
   } else {
-    $(".header__top").removeClass('display-none');
-    $(".header__bottom").removeClass('header__bottom--active');
+    $(".header__top").removeClass("display-none");
+    $(".header__bottom").removeClass("header__bottom--active");
   }
 });
-$('.workwith-slider').slick({
+$(".workwith-slider").slick({
   arrows: false,
-  slidesToShow:6,
+  slidesToShow: 6,
   responsive: [
     {
       breakpoint: 1100,
@@ -17,7 +17,32 @@ $('.workwith-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         // dots: true
-      }
+      },
     },
-  ]
-})
+  ],
+});
+
+// $(function () {
+  // $(".accordion__title-wrapper").on("click", function () {
+  //   if ((this).hasClass("active")) {
+  //   } else {
+  //     $(".accordion__title-wrapper").not($(this)).removeClass("active");
+  //     $(".accordion__text").not($(this).next()).slideUp(300);
+  //     $(this).toggleClass("active").next().slideDown(300);
+  //   }
+  // });
+
+  $('.accordion__title-wrapper').on("click", function () {
+  
+    var $this = $(this);
+  
+    if ($this.hasClass('active')) {
+        $this.removeClass('active');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('.accordion__title-wrapper').removeClass('active');
+        $this.parent().parent().find('.accordion__title-wrapper').next().slideUp(350);
+        $this.addClass('active');
+        $this.next().slideToggle(350);
+    }
+});
